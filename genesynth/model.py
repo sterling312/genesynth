f517cc1e2fa40ae8e8c2f16fcfc85cc42e189aad
Gang Huang <doc.n.try@gmail.com>
2022-10-29 21:57:24 -0400

    fix line wrap

--- genesynth/model.py
+++ genesynth/model.py
@@ -120,8 +120,9 @@ class JsonDataModel(BaseDataModel):
         filenames = {node.name: node._file for node in nodes}
         async with self._filename(path) as fh:
             for lines in iterate_lines(*filenames.values()):
-                # TODO add support for array
-                record = {key: json.loads(value) if isinstance(node, BaseMapFixture) else value for node, key, value in zip(nodes, filenames.keys(), lines)}
+                # TODO add support for array and ad support for structure other than json
+                record = {key: json.loads(value) if isinstance(node, BaseMapFixture) else value 
+                            for node, key, value in zip(nodes, filenames.keys(), lines)}
                 fh.write(json.dumps(record, default=lambda x: x.decode('ascii')).encode('utf-8'))
                 fh.write(b'\n')
                 await asyncio.sleep(0)
