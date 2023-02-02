762c4417724b8de8cd22a75bb9de095d455c2130
Gang Huang <doc.n.try@gmail.com>
2023-02-02 01:08:13 -0500

    update tests

--- genesynth/cli.py
+++ genesynth/cli.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
+import os
 import asyncio
 import click
 from genesynth.orchestration import *
 
-def run(filename, output):
+def run(filename, output=None):
+    if output is None:
+        output, ext = os.path.splitext(os.path.basename(filename))
     pipe = Orchestration.read_yaml(filename)
     pipe.run()
     root = pipe.graph.root.pop()
@@ -12,9 +15,7 @@ def run(filename, output):
 @click.command()
 @click.option('--filename', help='filename')
 @click.option('--output', default=None, help='output file')
-def main(filename, output=None):
-    if output is None:
-        output, ext = os.path.splitext(os.path.basename(filename))
+def main(filename, output):
     run(filename, output)
 
 if __name__ == '__main__':
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,6 +3,6 @@ import os
 from pytest import fixture
 from genesynth.cli import *
 
-def test_test():
+def test_run_out():
     run('tests/test.yaml', 'tests/out')
     assert os.path.isfile('tests/out')
