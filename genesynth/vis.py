db792c9476f7357d3ca3ee54828a965a917f6472
Gang Huang <doc.n.try@gmail.com>
2023-03-29 20:16:32 +0200

    add relabel support

--- genesynth/vis.py
+++ genesynth/vis.py
@@ -1,9 +1,12 @@
 from pyvis.network import Network
 
-def render(G, output='graph.html', height=600, width=800, **params):
+def render(G, output='graph.html', height=600, width=800, relabel=False, **params):
     net = Network(height=height, width=width, directed=True, **params)
-    mapping = {n: n.name for n in G}
-    net.from_nx(nx.relabel_nodes(G, mapping))
+    if relabel:
+        mapping = {n: n.name for n in G}
+        net.from_nx(nx.relabel_nodes(G, mapping))
+    else:
+        net.from_nx(G)
     net.show(output)
 
 
