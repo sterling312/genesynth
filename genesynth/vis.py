94906827b4d935fe44e7043044860e51cbeb4639
Gang Huang <doc.n.try@gmail.com>
2023-03-29 20:17:12 +0200

    default relabel to true

--- genesynth/vis.py
+++ genesynth/vis.py
@@ -1,6 +1,6 @@
 from pyvis.network import Network
 
-def render(G, output='graph.html', height=600, width=800, relabel=False, **params):
+def render(G, output='graph.html', height=600, width=800, relabel=True, **params):
     net = Network(height=height, width=width, directed=True, **params)
     if relabel:
         mapping = {n: n.name for n in G}
