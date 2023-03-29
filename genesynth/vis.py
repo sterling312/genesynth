2256f2cdaf22b9fb1843a14073fde205551e0797
Gang Huang <doc.n.try@gmail.com>
2023-03-29 20:14:19 +0200

    update render

--- genesynth/vis.py
+++ genesynth/vis.py
@@ -1,7 +1,7 @@
 from pyvis.network import Network
 
-def render(G, output='graph.html', **params):
-    net = Network(directed=True, **params)
+def render(G, output='graph.html', height=600, width=800, **params):
+    net = Network(height=height, width=width, directed=True, **params)
     mapping = {n: n.name for n in G}
     net.from_nx(nx.relabel_nodes(G, mapping))
     net.show(output)
