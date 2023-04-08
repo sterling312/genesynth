dbe3318188ce8fc7b3efc1baf5a6ffa11ecce600
Gang Huang <doc.n.try@gmail.com>
2023-04-08 19:24:26 -0400

    add print output

--- genesynth/cli.py
+++ genesynth/cli.py
@@ -7,14 +7,18 @@ from genesynth.orchestration import *
 parser = argparse.ArgumentParser()
 parser.add_argument('--filename', required=True, help='filename')
 parser.add_argument('--output', help='filename')
+parser.add_argument('--stdout', action='store_true', help='print output to stdout')
 
-def main(filename, output=None):
+def main(filename, output=None, stdout=False):
     if output is None:
         output, ext = os.path.splitext(os.path.basename(filename))
     pipe = Orchestration.read_yaml(filename)
     pipe.run()
     asyncio.run(pipe.root.save(output))
+    if stdout:
+        with open(output) as fh:
+            print(fh.read())
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    main(args.filename, args.output)
+    main(args.filename, args.output, args.stdout)
