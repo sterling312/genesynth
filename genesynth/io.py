143777cfb16f4d90f57bba1c1a390cc52c0a0933
Gang Huang <doc.n.try@gmail.com>
2023-08-13 23:28:33 -0400

    default array

--- genesynth/io.py
+++ genesynth/io.py
@@ -84,8 +84,8 @@ def schema_to_graph(G, fullname, params, size=0, root='root'):
         node = BaseForeign.from_params(name=fullname, graph=G, depends_on=depends_on, metadata=metadata, **metadata)
     elif container == 'array':
         container = datatypes[type].from_params(name=fullname, metadata=metadata, is_array=True, **metadata)
-        node = datatypes['json'].from_params(name=fullname, metadata=metadata, is_array=True, **metadata)
-        #node = datatypes['array'].from_params(name=fullname, metadata=metadata, is_array=True, **metadata)
+        #node = datatypes['json'].from_params(name=fullname, metadata=metadata, is_array=True, **metadata)
+        node = datatypes['array'].from_params(name=fullname, metadata=metadata, is_array=True, **metadata)
     else:
         node = datatypes[type].from_params(name=fullname, metadata=metadata, **metadata)
     properties = params.get('properties')
@@ -99,8 +99,8 @@ def schema_to_graph(G, fullname, params, size=0, root='root'):
             node.children = Hashabledict(children)
         else:
             container.children = Hashabledict(children)
-            node.children = Hashabledict({container: container})
-            #node.children = (container, )
+            #node.children = Hashabledict({container: container})
+            node.children = (container, )
         # add node to graph after setting data field children
         for child in children.values():
             G.add_edge(node, child) # add relationship type here
