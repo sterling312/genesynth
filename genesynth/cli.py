9dd84673607725d873e6864d4472bc44299458bd
Gang Huang <doc.n.try@gmail.com>
2023-08-27 20:55:37 -0400

    cli: default to stdout

--- genesynth/cli.py
+++ genesynth/cli.py
@@ -8,14 +8,12 @@ from genesynth.orchestration import *
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--filename', required=True, help='schema file')
 parser.add_argument('-o', '--output', help='output filename if given. defaults to input filename without extension')
-parser.add_argument('--stdout', action='store_true', help='print output to stdout')
+parser.add_argument('--stdout', action='store_true', help='DEPRECATED print output to stdout')
 
-def main(filename, output=None, stdout=False):
-    if output is None:
-        output, ext = os.path.splitext(os.path.basename(filename))
+def main(filename, output=None):
     pipe = Orchestration.read_config(filename)
     pipe.run()
-    if stdout:
+    if output is None:
         asyncio.run(pipe.root.save())
         with open(pipe.root._file) as fh:
             for line in fh:
@@ -25,4 +23,4 @@ def main(filename, output=None, stdout=False):
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    main(args.filename, args.output, args.stdout)
+    main(args.filename, args.output)
